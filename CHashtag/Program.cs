a97cbdec73f45ebdd4a3a6e13fb6a70f9cec6787
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-18 01:44:38 +0800

    chore: removed bin and added git ignore

--- CHashtag/Program.cs
+++ CHashtag/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build(); // this is start an instant of building the build
-
+//dotnet run 
+//dotnet build
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
