8eeda2d766aa8878c7488edd5b4f38f0d52b5975
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-18 01:59:15 +0800

    refactor: Readme and add games.http

--- CHashtag/Program.cs
+++ CHashtag/Program.cs
@@ -3,5 +3,10 @@ var app = builder.Build(); // this is start an instant of building the build
 //dotnet run 
 //dotnet build
 app.MapGet("/", () => "Hello World!");
+// DTO - Data transfer Object carries data between operations
+
+
+
+
 
 app.Run();
