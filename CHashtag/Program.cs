69dbc26b3d5d54ec564d88a42207da173d64fbeb
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-18 22:21:47 +0800

    feat: add Fetch Route and then Fetch Route by ID

--- CHashtag/Program.cs
+++ CHashtag/Program.cs
@@ -7,7 +7,20 @@ var app = builder.Build(); // this is start an instant of building the build
 //dotnet build
 
 
-List<GameDto> games = [new(1, "sample1", "sample2nd", 199, new DateOnly(1992, 8, 12))];
+List<GameDto> games =
+[
+    new(1, "sample1", "sample2nd", 199.9M, new DateOnly(1992, 8, 12)),
+    new(2, "sample2", "sample3nd", 199.9M, new DateOnly(1992, 8, 12)),
+];
+
+//Get  Games endpoint
+app.MapGet("games", () => games);
+
+//get games by id
+
+
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+
 app.MapGet("/", () => "Hello World!");
 
 // DTO - Data transfer Object carries data between operations
