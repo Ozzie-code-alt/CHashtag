aae3c5a618e24b7feb5b7c5193ddbcdf783ec113
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-10-13 18:46:06 +0800

    refactor: minor updates in editor config and program cs

--- CHashtag/Program.cs
+++ CHashtag/Program.cs
@@ -1,87 +1,8 @@
-using CHashtag.DTOs;
+using CHashtag.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build(); // this is start an instant of building the build
-const string getGameEndpointName = "GetGame";
-
-//dotnet run
-//dotnet build
-
-
-// removed the ID
-List<GameDto> games =
-[
-    new(1, "sample1", "sample2nd", 199.9M, new DateOnly(1992, 8, 12)),
-    new(2, "sample2", "sample3nd", 199.9M, new DateOnly(1992, 8, 12)),
-];
-
-//Get  Games endpoint
-app.MapGet("games", () => games);
-
-//get games by id
-app.MapGet(
-        "games/{id}",
-        (int id) =>
-        {
-            GameDto? game = games.Find(game => game.Id == id);
-
-            //Look into this - more of like an appropriate handling of no Games or invalid params id
-            return game is null ? Results.NotFound() : Results.Ok(game);
-        }
-    )
-    .WithName(getGameEndpointName);
-
-app.MapGet("/", () => "Hello World!");
-
-// POST Request  / games
-app.MapPost(
-    "games",
-    (CreateGameDto newGame) =>
-    {
-        GameDto game =
-            new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
-
-        games.Add(game);
-
-        return Results.CreatedAtRoute(getGameEndpointName, new { id = game.Id }, game);
-    }
-);
-
-//Put Endpoint
-app.MapPut(
-    "games/{id}",
-    (int id, UpdateGameDto updateGame) =>
-    {
-        //find index first
-        //what happends if there is no game ?
-        var index = games.FindIndex(game => game.Id == id);
-
-        if (index == -1) // Out of bounds
-        {
-            return Results.NotFound();
-        }
-
-        games[index] = new GameDto(
-            id,
-            updateGame.Name,
-            updateGame.Genre,
-            updateGame.Price,
-            updateGame.ReleaseDate
-        );
-        return Results.NoContent();
-    }
-);
-
-//Delete Endpoint
-app.MapDelete(
-    "games/{id}",
-    (int id) =>
-    {
-        games.RemoveAll(game => game.Id == id);
-
-        return Results.NoContent();
-    }
-);
+app.MapGamesEndpoints();
 
 // DTO - Data transfer Object carries data between operations
 app.Run();
